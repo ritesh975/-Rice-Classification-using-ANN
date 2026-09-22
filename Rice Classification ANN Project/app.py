65a1d19fe07a4821e9ef72aaf382b7b7f72eadc5
𝗥𝗶𝘁𝗲𝘀𝗵 𝗸𝘂𝗺𝗮𝗿 𝗸𝗮𝘀𝗮𝘂𝗱𝗵𝗮𝗻 <RITESHKASAUDHAN975@GMAIL.COM>
2026-09-22 17:28:20 +0530

    Update app.py

--- Rice Classification ANN Project/app.py
+++ Rice Classification ANN Project/app.py
@@ -4,7 +4,6 @@ import joblib
 
 from tensorflow.keras.models import load_model
 
-
 # --------------------------------------------------
 # Page Configuration
 # --------------------------------------------------
